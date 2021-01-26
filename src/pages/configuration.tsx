7dd8f59da6f46de1472f7663ee57e6f85023ed8b
Krzysztof Wolski <krzysztof.k.wolski@gmail.com>
2021-01-26 23:47:34 +0100

    Clean up template files

---
+++ src/pages/configuration.tsx
@@ -0,0 +1,9 @@
+import Layout from '../components/Layout'
+
+const ConfigurationPage = () => (
+  <Layout title="Configuration">
+    <h1>⚙️</h1>
+  </Layout>
+)
+
+export default ConfigurationPage
---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import Layout from '../components/Layout'
+
+const IndexPage = () => (
+  <Layout title="Home">
+    <h1>👋</h1>
+  </Layout>
+)
+
+export default IndexPage
